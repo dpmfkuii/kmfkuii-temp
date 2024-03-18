8ded7fef0de86158d6be14a171494276af169ebe
hailinzparra <hailinzparra@gmail.com>
2024-03-19 02:59:08 +0700

    removed built js

--- public/js/endform.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var template_text_p = document.querySelector('#template_text');
-var template_text_param = new URLSearchParams(window.location.search).get('template_text') || '';
-template_text_p.innerHTML = "TEMPLATE TEXT: <strong>\"".concat(template_text_param, "\"</strong>");
-var link_wa = document.querySelector('#link_wa');
-link_wa.href = "https://wa.me/6282186647658?text=".concat(window.encodeURI(template_text_param));
