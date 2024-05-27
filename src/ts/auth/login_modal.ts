80f9b2359740fb0eebf9953beb1edd7d65c899b0
hailinzparra <hailinzparra@gmail.com>
2024-05-27 14:33:53 +0700

    toggle hide login input

--- src/ts/auth/login_modal.ts
+++ src/ts/auth/login_modal.ts
@@ -2,18 +2,36 @@
     const modal = dom.q<'div'>('#auth_login_modal')!
     const form = dom.q<'form'>('#auth_login_modal form')!
     const input_uid = dom.qe<'input'>(form, 'input[name="uid"]')!
+    const input_uid_group = input_uid.parentElement!
     const input_uid_invalid_feedback = dom.qe<'div'>(form, '.invalid-feedback')!
     const button_close = dom.qe<'button'>(form, '.modal-footer button[data-bs-dismiss="modal"]')!
     const button_submit = dom.qe<'button'>(form, 'button[type="submit"]')!
     const a_lupa_uid = dom.qe<'a'>(form, 'a[aria-label="Lupa UID"]')!
     const a_daftar = dom.qe<'a'>(form, 'a[aria-label="Daftar"]')!
+    const checkbox_hide_uid = dom.q<'input'>('#login_modal_hide_uid_input')!
+    const label_hide_uid = dom.q<'label'>('label[for="login_modal_hide_uid_input"]')!
+
+    checkbox_hide_uid.addEventListener('change', () => {
+        if (checkbox_hide_uid.checked) {
+            label_hide_uid.innerHTML = '<i class="fa-solid fa-eye"></i>'
+            input_uid.placeholder = '~ UID ~'
+            input_uid.autocomplete = 'off'
+        }
+        else {
+            label_hide_uid.innerHTML = '<i class="fa-solid fa-eye-slash"></i>'
+            input_uid.placeholder = 'UID'
+            input_uid.autocomplete = 'on'
+        }
+    })
 
     modal.addEventListener('shown.bs.modal', () => {
         input_uid.focus()
     })
 
     modal.addEventListener('hidden.bs.modal', () => {
+        input_uid_group.classList.remove('is-invalid')
         input_uid.classList.remove('is-invalid')
+        input_uid.classList.add('border-end-0')
     })
 
     form.addEventListener('submit', async ev => {
@@ -32,7 +50,9 @@
         dom.disable(...disabled_elements)
 
         button_submit.innerHTML = '<span class="spinner-border spinner-border-sm" aria-hidden="true"></span> Masuk'
+        input_uid_group.classList.remove('is-invalid')
         input_uid.classList.remove('is-invalid')
+        input_uid.classList.add('border-end-0')
 
         await common.sleep(100)
 
@@ -42,7 +62,9 @@
                 button_submit.innerHTML = '<i class="fa-solid fa-arrow-right-to-bracket"></i> Masuk'
 
                 input_uid_invalid_feedback.textContent = 'Maaf, UID tidak ditemukan. Silakan periksa kembali.'
+                input_uid_group.classList.add('is-invalid')
                 input_uid.classList.add('is-invalid')
+                input_uid.classList.remove('border-end-0')
 
                 input_uid.focus()
             }
