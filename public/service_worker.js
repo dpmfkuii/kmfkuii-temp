f874c069a8967a0e133095e7001cd2ef28f6523e
hailinzparra <hailinzparra@gmail.com>
2024-06-18 15:04:16 +0700

    add manifest items

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,6 +1,8 @@
+const current_cache_name = 'web-km-fkuii-v0.5a'
+
 self.addEventListener('install', ev => {
     ev.waitUntil(
-        caches.open('web-km-fkuii-v0.5a').then(cache => {
+        caches.open(current_cache_name).then(cache => {
             cache.addAll([
                 '/',
                 '/index.html',
@@ -29,6 +31,18 @@ self.addEventListener('fetch', ev => {
     ev.respondWith(
         caches.match(ev.request).then(res => {
             return res || fetch(ev.request)
+        })
+    )
+})
+
+self.addEventListener('activate', ev => {
+    ev.waitUntil(
+        caches.keys().then(names => {
+            for (let name of names) {
+                if (name !== current_cache_name) {
+                    caches.delete(name)
+                }
+            }
         })
     )
 })
